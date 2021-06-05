e3ce416c93221323f04283cb28733aa4fc20c082
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-06-05 14:36:19 -0700

    Use portable st_mtime over st_mtim in src/compiler/spirv/nir_load_libclc.c.

--- src/compiler/spirv/nir_load_libclc.c
+++ src/compiler/spirv/nir_load_libclc.c
@@ -123,7 +123,7 @@ open_clc_data(struct clc_data *clc, unsigned ptr_bit_size)
       struct mesa_sha1 ctx;
       _mesa_sha1_init(&ctx);
       _mesa_sha1_update(&ctx, clc->file->sys_path, strlen(clc->file->sys_path));
-      _mesa_sha1_update(&ctx, &stat.st_mtim, sizeof(stat.st_mtim));
+      _mesa_sha1_update(&ctx, &stat.st_mtime, sizeof(stat.st_mtime));
       _mesa_sha1_final(&ctx, clc->cache_key);
 
       clc->fd = fd;
